478fd8aa01339c72e574829e68b39401a91dc812
khy07181 <khy07181@gmail.com>
2022-05-10 00:21:05 +0900

    test name

---
+++ src/test/java/com/example/thejavatest/StudyTest.java
@@ -0,0 +1,44 @@
+package com.example.thejavatest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StudyTest {
+
+    @Test
+    @DisplayName("create")
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @DisplayName("create1")
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each");
+    }
+
+}
