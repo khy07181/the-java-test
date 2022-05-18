6152f5ac1447dde62eb25f4706c9a6f8929ebeae
hayoung <khy07181@gmail.com>
2022-05-19 00:22:00 +0900

    BDDMockito

--- src/test/java/com/example/thejavatest/Study/StudyServiceTest.java
+++ src/test/java/com/example/thejavatest/Study/StudyServiceTest.java
@@ -5,14 +5,16 @@ import com.example.thejavatest.domain.Member;
 import com.example.thejavatest.domain.Study;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -25,6 +27,7 @@ class StudyServiceTest {
 
     @Test
     void createNewStudy() {
+        // Given
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
 
@@ -34,22 +37,19 @@ class StudyServiceTest {
 
         Study study = new Study(10, "테스트");
 
-        when(memberService.findById(1L)).thenReturn(Optional.of(member));
-        when(studyRepository.save(study)).thenReturn(study);
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+        given(studyRepository.save(study)).willReturn(study);
 
+        // When
         studyService.createNewStudy(1L, study);
+
+        // Then
         assertEquals(member, study.getOwner());
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).should(times(1)).notify(member);
 
-        // 호출 횟수 검증
-        verify(memberService, times(1)).notify(study);
-        verify(memberService, times(1)).notify(member);
-        verify(memberService, never()).validate(any());
-
-        // 호출 순서 검증
-        InOrder inOrder = inOrder(memberService);
-        inOrder.verify(memberService).notify(study);
-        inOrder.verify(memberService).notify(member);
-
+        // 특정 시점 이후에 아무 일도 벌어지지 않았는지
+//        then(memberService).shouldHaveNoMoreInteractions();
     }
 
 }
